4e3ff442155547269e55894a80bba20d2b951a8b
Pavan8374 <rpavank2002@gmail.com>
2025-03-05 10:57:17 +0530

    initial push for this repo

--- src/APIGateway/Controllers/WeatherForecastController.cs
+++ src/APIGateway/Controllers/WeatherForecastController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ApiGateway.Controllers
 {
+    /// <summary>
+    /// 
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
