8c25f7baaeffad4d95419523132b47012f8edfd9
Pavan8374 <rpavank2002@gmail.com>
2025-04-29 17:59:37 +0530

    Added summary to user created event class

--- src/Services/IdentityService/Identity.Domain/Events/Users/UserCreatedEvent.cs
+++ src/Services/IdentityService/Identity.Domain/Events/Users/UserCreatedEvent.cs
@@ -2,11 +2,26 @@
 
 namespace Identity.Domain.Events.Users
 {
+    /// <summary>
+    /// User created event
+    /// </summary>
     public class UserCreatedEvent : DomainEvent
     {
+        /// <summary>
+        /// User identity
+        /// </summary>
         public Guid UserId { get; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
         public string Email { get; }
 
+        /// <summary>
+        /// User created event
+        /// </summary>
+        /// <param name="userId">User identity</param>
+        /// <param name="email">Email</param>
         public UserCreatedEvent(Guid userId, string email)
         {
             UserId = userId;
